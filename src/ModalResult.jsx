7cd5d6d7792e18548ece4fcbf1a65deebfa2e9c0
leo verdugo <leobardo@twozer00.dev>
2024-01-06 02:43:30 -0700

    minor changes

--- src/ModalResult.jsx
+++ src/ModalResult.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 export default function ModalResult({ result, points, reset }) {
     return (
         <div className='w-full h-full bg-white/10 backdrop-blur absolute top-0 left-0 z-50 flex flex-col justify-center items-center' >
-            <div className='border rounded-lg w-1/2 h-[400px] bg-white shadow-lg'>
+            <div className='border rounded-lg w-11/12 sm:w-1/2 h-[400px] bg-white shadow-lg'>
                 <div className='flex flex-col justify-center items-center text-black h-full p-4'>
-                    <h1 className='text-4xl font-bold'>{result ? "Good game!" : "Congratulations!"}</h1>
+                    <h1 className='text-4xl font-bold text-center'>{result ? "Good game!" : "Congratulations!"}</h1>
                     {result ?
                         <p className='text-md text-center'>You lost the game!</p>
                         :
